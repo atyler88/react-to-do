6b10830b060d73af2ed24d4af78d9265644df56b
atyler88 <a.tyler88@gmail.com>
2018-12-06 02:41:59 -0600

    added a delete function for removing todos

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -4,8 +4,16 @@ class App extends Component {
   state = {
     todos: [
       {id: 1, content: 'buy some milk'},
-      {id: 1, content: 'play mario kart'}
+      {id: 2, content: 'play mario kart'}
     ]
+  }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    })
   }
   render() {
     return (
@@ -14,7 +22,7 @@ class App extends Component {
         <svg id='color-fill' xmlns='http://www.w3.org/2000/svg' height='300'>
           <polygon className='hex' points='300,150 225,280 75,280 0,150 75,20 225,20'fill='#fa5' transform="rotate(90, 150, 150)"/>
         </svg></h1>
-        <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     );
   }
